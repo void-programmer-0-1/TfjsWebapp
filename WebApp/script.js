4f560882971eb9a9f52447ed3b25bd0543f68400
santoshm <santoshm@digikriya.com>
2021-08-17 20:03:12 +0530

    front end over

--- WebApp/script.js
+++ WebApp/script.js
@@ -3,11 +3,8 @@ async function predict()
 {
     user_image = document.getElementById("user-image");
 
-    let width = user_image.width;
-    let height = user_image.height;
-
     let input_tensor = tf.browser.fromPixels(user_image);
-    input_tensor.print();
+    //input_tensor.print();
 
     const model = await tf.loadLayersModel("../cat_vs_dog_tfjs/model.json");
     input_tensor = input_tensor.reshape([1,224,224,3]);
@@ -16,9 +13,11 @@ async function predict()
     
     if(prediction > 0.5){
         console.log("DOG");
+        document.getElementById("result").innerText = "Boww Boww";
     }
     else{
         console.log("CAT");
+        document.getElementById("result").innerText = "Muahhh";
     }
 }
 
